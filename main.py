9480357a6d214f6403582cb2e9fe5df250ef3537
LetC0de <abhishekjiiofficial86@gmail.com>
2026-07-07 20:39:52 +0530

    Add sorting functionality for patients by height, weight, or BMI

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI , Path , HTTPException
+from fastapi import FastAPI , Path , Query , HTTPException
 import json
 
 app = FastAPI()
@@ -34,3 +34,23 @@ def view_patients(patients_id : str = Path(...,description = "Id of patient" ,ex
     if patients_id in data:
         return data[patients_id]
     raise HTTPException(status_code = 404 , detail='Patient not found')
+
+
+@app.get("/sort")
+def sort_patients(sort_by: str = Query(..., description= 'sort on the basis of height, weight, or bmi'), order_by: str = Query('asc',description= 'sort in asc or desc order')):
+
+    valid_fields = ['height','weight','bmi']
+
+    if sort_by not in valid_fields:
+        raise HTTPException(status_code= 400, detail=f'invalid field select from{valid_fields}')
+    
+    if order_by not in ['asc','desc']:
+        raise HTTPException(status_code= 400 , detail='invalid field select from asc and desc')
+    
+    data = load_data()
+
+    reverse = True if order_by == 'desc' else False 
+
+    sorted_data = sorted(data.values(), key=lambda x:x.get(sort_by, 0), reverse=reverse)
+
+    return sorted_data
