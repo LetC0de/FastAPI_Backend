aa8f8fe0008131b9fe5c81fd95c33b886506f0db
LetC0de <abhishekjiiofficial86@gmail.com>
2026-07-08 21:00:39 +0530

    Add optional fields to PatientUpdate model for flexible patient data updates

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI , Path , Query , HTTPException
 from fastapi.responses import JSONResponse
 from pydantic import BaseModel,Field,computed_field
-from typing import Annotated,Literal
+from typing import Annotated,Literal,Optional
 import json
 
 app = FastAPI()
@@ -33,6 +33,16 @@ class patient(BaseModel):
             return "Overweight"
         else:
             return "Obese"
+
+
+class PatientUpdate(BaseModel):
+    name: Annotated[Optional[str], Field(default=None)]
+    city: Annotated[Optional[str], Field(default=None)]
+    age: Annotated[Optional[int], Field(default=None, gt=0)]
+    gender: Annotated[Optional[Literal['male', 'female']], Field(default=None)]
+    height: Annotated[Optional[float], Field(default=None, gt=0)]
+    weight: Annotated[Optional[float], Field(default=None, gt=0)]
+
 
 
 
