ab326d328cbba8275190746607b83df4394b8e66
LetC0de <abhishekjiiofficial86@gmail.com>
2026-07-08 17:55:08 +0530

    Add additional fields to patient model and update patient_info for data insertion

--- file_pydantic.py
+++ file_pydantic.py
@@ -1,13 +1,22 @@
 from pydantic import BaseModel
+from typing import List , Dict
 
 class patient(BaseModel):
     name : str
     age : int 
+    weight : float
+    married : bool
+    allergies: List[str]
+    contact_details : Dict[str , str]
 
 
 patient_info = {
     "name" : "Ananya",
-    "age" : 28
+    "age" : 28,
+    "weight" : 90.0,
+    "married" : False,
+    "allergies" : ["peanuts", "eggs", "dust"],
+    "contact_details" : {"email" : "ananyaverma@.com", "phone" : "9876543210"}
 }
 
 patient1 = patient(**patient_info)
@@ -15,6 +24,10 @@ patient1 = patient(**patient_info)
 def insert_patient_data(patient : patient):
     print(patient.name)
     print(patient.age)
+    print(patient.weight)
+    print(patient.married)
+    print(patient.allergies)
+    print(patient.contact_details)
     print("inserted")
 
 
