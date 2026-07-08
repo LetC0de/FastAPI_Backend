5fd0d6af8b291780f52b928517765a24acc06103
LetC0de <abhishekjiiofficial86@gmail.com>
2026-07-08 18:13:00 +0530

    Refactor patient model to include email and URL fields, and update patient_info accordingly

--- file_pydantic.py
+++ file_pydantic.py
@@ -1,22 +1,26 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr , AnyUrl
 from typing import List , Dict , Optional
 
 class patient(BaseModel):
     name : str
     age : int 
+    email : EmailStr
     weight : float
-    married : bool
+    married : bool = False
     allergies: Optional[List[str]] = None
     contact_details : Dict[str , str]
+    url : AnyUrl
 
 
 patient_info = {
     "name" : "Ananya",
     "age" : 28,
+    "email" : "ananyaverma@gmail.com",
     "weight" : 90.0,
     "married" : False,
     "allergies" : ["peanuts", "eggs", "dust"],
-    "contact_details" : {"email" : "ananyaverma@.com", "phone" : "9876543210"}
+    "contact_details" : {"phone" : "9876543210"},
+    "url" : "https://google.com"
 }
 
 patient1 = patient(**patient_info)
