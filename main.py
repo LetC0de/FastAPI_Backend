6070a9b53d7d407eb5cc9243f7933adc0d8d40cd
LetC0de <abhishekjiiofficial86@gmail.com>
2026-07-10 13:36:54 +0530

    Implement patient deletion endpoint and clean up update_patient function

--- main.py
+++ main.py
@@ -142,7 +142,7 @@ def update_patient(patient_id: str, patient_update: PatientUpdate):
     #existing_patient_info -> pydantic object -> updated bmi + verdict
     existing_patient_info['id'] = patient_id
     patient_pydandic_obj = patient(**existing_patient_info)
-    
+
     #-> pydantic object -> dict
     existing_patient_info = patient_pydandic_obj.model_dump(exclude='id')
 
@@ -153,3 +153,20 @@ def update_patient(patient_id: str, patient_update: PatientUpdate):
     save_data(data)
 
     return JSONResponse(status_code=200, content={'message':'patient updated'})
+
+
+@app.delete('/delete/{patient_id}')
+def delete_patient(patient_id: str):
+
+    # load data
+    data = load_data()
+
+    if patient_id not in data:
+        raise HTTPException(status_code=404, detail='Patient not found')
+    
+    del data[patient_id]
+
+    save_data(data)
+
+    return JSONResponse(status_code=200, content={'message':'patient deleted'})
+
