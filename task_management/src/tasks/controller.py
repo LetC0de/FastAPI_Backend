f2312c592c3906127845eb7518bf340faf94f048
LetC0de <abhishekjiiofficial86@gmail.com>
2026-07-11 18:42:28 +0530

    Add get_one_task endpoint and update router to include task retrieval

--- task_management/src/tasks/controller.py
+++ task_management/src/tasks/controller.py
@@ -1,6 +1,7 @@
 from src.tasks.dtos import Tastschema
 from sqlalchemy.orm import Session
 from src.tasks.models import TaskModel
+from fastapi import HTTPException
 
 def create_task(body : Tastschema,db:Session):
     data = body.model_dump()
@@ -20,3 +21,13 @@ def create_task(body : Tastschema,db:Session):
 def get_tasks(db:Session):
     tasks = db.query(TaskModel).all()
     return {"status":"All tasks","data":tasks}
+
+
+
+def get_one_task(task_id:int,db:Session):
+    task = db.query(TaskModel).get(task_id)
+    
+    if not task:
+        raise HTTPException(status_code=404, detail="Task not found")
+    
+    return {"status":"Task fetched successfully","data":task}
