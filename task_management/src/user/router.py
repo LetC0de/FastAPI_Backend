d53ed9a84968518905140028ff43c7304005659c
LetC0de <abhishekjiiofficial86@gmail.com>
2026-07-13 21:04:26 +0530

    Add UserResponseSchema to registration endpoint response

--- task_management/src/user/router.py
+++ task_management/src/user/router.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter, Depends, status
-from src.user.dtos import UserSchema
+from src.user.dtos import UserSchema, UserResponseSchema
 from sqlalchemy.orm import Session
 from src.utils.db import get_db
 from src.user import controller
@@ -7,6 +7,6 @@ from src.user import controller
 user_router = APIRouter(prefix="/user")
 
 
-@user_router.post("/register", status_code=status.HTTP_201_CREATED)
+@user_router.post("/register", response_model=UserResponseSchema, status_code=status.HTTP_201_CREATED)
 def register(body: UserSchema, db: Session = Depends(get_db)):
     return controller.register(body, db)
