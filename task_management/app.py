f261d10910e974386268734a689b94f64ae37468
LetC0de <abhishekjiiofficial86@gmail.com>
2026-07-13 20:33:11 +0530

    Include user router in the FastAPI application

--- task_management/app.py
+++ task_management/app.py
@@ -1,9 +1,11 @@
 from fastapi import FastAPI
 from src.utils.db import base,engine
 from src.tasks.router import task_router
+from src.user.router import user_router
 
 
 base.metadata.create_all(engine)
 
 app = FastAPI(title="Task Management App")
 app.include_router(task_router)
+app.include_router(user_router)
