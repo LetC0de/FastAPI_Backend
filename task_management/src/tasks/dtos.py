879201d6eee32a30763c328b565cc2250164df67
LetC0de <abhishekjiiofficial86@gmail.com>
2026-07-13 19:22:04 +0530

    Add new data models for task schema and response schema

--- task_management/src/tasks/dtos.py
+++ task_management/src/tasks/dtos.py
@@ -4,3 +4,10 @@ class Tastschema(BaseModel):
     title : str
     description : str
     is_completed : bool = False
+
+
+class TastResponseschema(BaseModel):
+    id : int
+    title : str
+    description : str
+    is_completed : bool
