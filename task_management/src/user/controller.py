c9d890e77ebb2bea7c25a9eea5fdc6d3331c0bed
LetC0de <abhishekjiiofficial86@gmail.com>
2026-07-13 20:35:34 +0530

    Add user registration function with UserSchema validation

---
+++ task_management/src/user/controller.py
@@ -0,0 +1,8 @@
+from src.user.dtos import UserSchema
+from sqlalchemy.orm import Session
+
+
+def register(body: UserSchema, db: Session):
+
+    print(body)
+    return {"message": "User registered successfully"}
