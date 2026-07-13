cd09c78ce55428827c63ef80cc197c2823d4ae8a
LetC0de <abhishekjiiofficial86@gmail.com>
2026-07-13 20:32:56 +0530

    Add user registration endpoint with UserSchema validation

---
+++ task_management/src/user/router.py
@@ -0,0 +1,12 @@
+from fastapi import APIRouter, Depends, status
+from src.user.dtos import UserSchema
+from sqlalchemy.orm import Session
+from src.utils.db import get_db
+from src.user import controller
+
+user_router = APIRouter(prefix="/user")
+
+
+@user_router.post("/register", status_code=status.HTTP_201_CREATED)
+def register(body: UserSchema, db: Session = Depends(get_db)):
+    return controller.register(body, db)
